950ecab3f7d3591c17afc44fc67152da779407a1
Kristijan Karanfilov <kar.kristijan@gmail.com>
2022-08-27 12:02:19 +0200

    init

---
+++ SEDC.WebApi.Class08/SEDC.WebApi.Class08.AdoNET/Controllers/NoteController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.WebApi.Class08.AdoNET.Models;
+using SEDC.WebApi.Class08.AdoNET.Services;
+
+namespace SEDC.WebApi.Class08.AdoNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Note>> GetAllNotes()
+        {
+            var noteService = new NoteService();
+
+            //var note = noteService.GetNoteByUserId(1, 2);
+            noteService.Add("NAUCI ADONET", "red", 5, 1);
+            var notes = noteService.GetAllNotes();
+
+            return Ok(notes);
+        }
+
+        [HttpGet("dapper")]
+        public ActionResult<Note> GetAllDapper()
+        {
+            var noteService = new NoteDapperService();
+
+            noteService.Add("dapper", "green", 5, 1);
+            //var notes = noteService.GetAll();
+
+            var note = noteService.GetByUserIdAndNoteId(1, 1);
+
+            return Ok(note);
+        }
+    }
+}
---
+++ SEDC.WebApi.Class08/SEDC.WebApi.Class08.AdoNET/Services/NoteService.cs
@@ -0,0 +1,108 @@
+﻿using SEDC.WebApi.Class08.AdoNET.Models;
+using System.Data.SqlClient;
+
+namespace SEDC.WebApi.Class08.AdoNET.Services
+{
+    public class NoteService
+    {
+        private readonly string connectionString = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=NotesApp_DB";
+
+        public IEnumerable<Note> GetAllNotes()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+
+            cmd.CommandText = "select * from dbo.Note";
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            var notes = new List<Note>();
+
+            while (reader.Read())
+            {
+                var note = new Note
+                {
+                    Id = (int)reader["Id"],
+                    Color = (string)reader["Color"],
+                    Text = (string)reader["Text"],
+                    Tag = (int)reader["Tag"],
+                    UserId = (int)reader["UserId"],
+                };
+                notes.Add(note);
+            }
+
+            connection.Close();
+            return notes;
+        }
+
+        public Note GetNoteByUserId(int userId, int noteId)
+        {
+            string sqlInjection = $@"0;insert into dbo.Note (Text, Color, tag, UserId) VALUES('Test inj', 'Red', 4, 1);";
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var cmd = new SqlCommand();
+                cmd.Connection = connection;
+
+                //cmd.CommandText = $"select * from dbo.Note where Id = {sqlInjection}";
+
+                cmd.CommandText = "select * from dbo.Note where Id = @id and UserId = @userId";
+                cmd.Parameters.AddWithValue("@id", noteId);
+                cmd.Parameters.AddWithValue("@userId", userId);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                var note = new Note();
+
+                while (reader.Read())
+                {
+                    note = new Note
+                    {
+                        Id = (int)reader["Id"],
+                        Color = (string)reader["Color"],
+                        Text = (string)reader["Text"],
+                        Tag = (int)reader["Tag"],
+                        UserId = (int)reader["UserId"],
+                    };
+                }
+
+                return note;
+            }
+        }
+
+        public void Add(string text, string color, int tag, int userId)
+        {
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    var cmd = new SqlCommand();
+                    cmd.Connection = connection;
+
+                    cmd.CommandText = "insert into dbo.Note (Text, Color, Tag, UserId) Values(@text, @color, @tag, @userId)";
+
+                    cmd.Parameters.AddWithValue("@text", text);
+                    cmd.Parameters.AddWithValue("@color", color);
+                    cmd.Parameters.AddWithValue("@tag", tag);
+                    cmd.Parameters.AddWithValue("@userId", userId);
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception();
+                }
+                finally
+                {
+
+                }
+            }
+        }
+    }
+}
